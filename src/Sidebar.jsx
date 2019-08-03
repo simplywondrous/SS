68b3de96b18bf1fb0abdc9a6cce7511967546ee2
Jasmine <workworkwang@gmail.com>
2019-08-02 19:16:11 -0500

    Removed unused file, saved sidebar changes

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { makeStyles } from "@material-ui/styles"
 
-// Sidebar
 import IconButton from "@material-ui/core/IconButton"
 import CloseIcon from "@material-ui/icons/ChevronLeft"
 import OpenIcon from "@material-ui/icons/ChevronRight"
