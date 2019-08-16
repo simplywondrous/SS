6d83f413e08b55dc8fbb7e3d797f5650cce453d9
Jasmine <workworkwang@gmail.com>
2019-08-16 18:40:48 -0500

    Moving forward with ItemPortal form

--- src/ItemPortal.jsx
+++ src/ItemPortal.jsx
@@ -1,14 +1,11 @@
-import React, { useReducer, useState } from "react"
-import { makeStyles } from "@material-ui/styles"
-import {
-  Card,
-  FormControl,
-  InputLabel,
-  Input,
-  Typography
-} from "@material-ui/core"
+import React, { useReducer, useState } from "react";
+import { makeStyles } from "@material-ui/styles";
+import { Card, TextField, Typography } from "@material-ui/core";
 
-import pic from "./pic.png"
+import IconButton from "@material-ui/core/IconButton";
+import CheckIcon from "@material-ui/icons/CheckCircleOutline";
+
+import pic from "./pic.png";
 
 const useStyles = makeStyles({
   root: {
@@ -31,39 +28,53 @@ const useStyles = makeStyles({
     flexDirection: "column",
     margin: "10px",
     height: "auto",
-    padding: "15px",
+    padding: "15px 5px",
     justifyContent: "space-between"
+  },
+  formColumnBlock: {
+    display: "flex",
+    flexDirection: "column",
+    margin: "10px 5px",
+    height: "auto",
+    padding: "15px 5px",
+    justifyContent: "flex-start"
   },
   image: {
     height: "200px",
     width: "200px",
     objectFit: "cover"
+  },
+  editing: {
+    margin: "5px 0px"
+  },
+  picker: {
+    margin: "10px 0px 5px 0px",
+    shrink: true
   }
-})
+});
 
 const ItemPortal = ({ item }) => {
   const initialState = {
     name: item.name ? item.name : "",
     image: item.image ? item.image : "",
     brand: item.brand ? item.brand : "",
-    year: item.year ? item.year : null,
     expiration: item.expiration ? item.expiration.toLocaleDateString() : "",
     openedOn: item.openedOn ? item.openedOn.toLocaleDateString() : "",
     finishedOn: item.finishedOn ? item.finishedOn.toLocaleDateString() : "-",
     drawer: item.drawer ? item.drawer : "",
     notes: item.notes ? item.notes : ""
-  }
+  };
 
   function reducer(state, input) {
-    let newState = { ...state }
-    newState[input.id] = input.value
+    let newState = { ...state };
+    newState[input.id] = input.value;
 
-    return newState
+    return newState;
   }
 
-  const [state, dispatch] = useReducer(reducer, initialState)
-  const [edit, setEdit] = useState(false)
-  const classes = useStyles()
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [edit, setEdit] = useState(true);
+  const classes = useStyles();
 
   return (
     <Card className={classes.root}>
@@ -71,86 +82,107 @@ const ItemPortal = ({ item }) => {
         <div className={classes.columnBlock}>
           <img className={classes.image} src={pic} />
         </div>
+        {!edit ? (
+          <>
+            <div className={classes.columnBlock}>
+              <div>
+                <Typography variant="subtitle1">{item.brand}</Typography>
+                <Typography variant="h4">{item.name}</Typography>
+              </div>
+              <div>
+                {item.expiration ? (
+                  <Typography variant="body2">
+                    Expires: {item.expiration.toLocaleDateString()}
+                  </Typography>
+                ) : null}
+                {item.openedOn ? (
+                  <Typography variant="body2">
+                    Opened: {item.openedOn.toLocaleDateString()}
+                  </Typography>
+                ) : null}
+                {item.finishedOn ? (
+                  <Typography variant="body2">
+                    Finished: {item.finishedOn.toLocaleDateString()}
+                  </Typography>
+                ) : null}
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={classes.formColumnBlock}>
+              <TextField
+                // variant="outlined"
+                label="Brand"
+                id="my-input"
+                className={classes.editing}
+                // style={{ marginTop: "0px" }}
+              />
+              <TextField
+                className={classes.editing}
+                // variant="outlined"
+                label="Name"
+                id="my-input"
+              />
+            </div>
+            <div className={classes.formColumnBlock}>
+              <TextField
+                id="date"
+                label="Expiration Date"
+                type="date"
+                className={classes.picker}
+                InputLabelProps={{ shrink: true }}
+                style={{ marginTop: "5px" }}
+              />
+              <TextField
+                id="date"
+                label="Opened On"
+                type="date"
+                className={classes.picker}
+                InputLabelProps={{ shrink: true }}
+              />
+              <TextField
+                id="date"
+                label="Finished On"
+                type="date"
+                className={classes.picker}
+                InputLabelProps={{ shrink: true }}
+              />
+            </div>
+          </>
+        )}
+
         <div className={classes.columnBlock}>
-          <div>
-            <EditableField editable={edit} handleChange={dispatch}>
-              [<Typography variant="h4">{item.name}</Typography>
-              <Input />]
-            </EditableField>
-            <Typography variant="subtitle1">
-              {item.brand}
-              {item.year ? ` - ${item.year}` : null}
-            </Typography>
-          </div>
-          <div>
-            {item.expiration ? (
-              <Typography variant="body2">
-                Expires: {item.expiration.toLocaleDateString()}
-              </Typography>
-            ) : null}
-            {item.openedOn ? (
-              <Typography variant="body2">
-                Opened: {item.openedOn.toLocaleDateString()}
-              </Typography>
-            ) : null}
-            {item.finishedOn ? (
-              <Typography variant="body2">
-                Finished: {item.finishedOn.toLocaleDateString()}
-              </Typography>
-            ) : null}
-          </div>
-          {/* <EditableField
-            id="name"
-            editable={edit}
-            label="Name"
-            value={state.name}
-            tip="Name of Product"
-            handleChange={dispatch}
-          />
-          <EditableField
-            id="brand"
-            editable={edit}
-            label="Brand"
-            value={state.brand}
-            tip="Product Brand"
-            handleChange={dispatch}
-          />
-          <EditableField
-            id="year"
-            editable={edit}
-            label="Year"
-            value={state.year}
-            tip="If known, the version of this product"
-            handleChange={dispatch}
-          /> */}
+          <button onClick={() => setEdit(!edit)}>Edit Toggle</button>
         </div>
-        <div className={classes.columnBlock} />
       </div>
     </Card>
-  )
-}
+  );
+};
 
-// Children dictate content, whereas editable dictates basic input style
-const EditableField = ({ children, editable, handleChange }) => {
-  return editable ? (
-    <FormControl>
-      <InputLabel htmlFor={`${id}`}>{label}</InputLabel>
-      <Input
-        id={`${id}`}
-        value={value}
-        onChange={e => handleChange({ id: e.target.id, value: e.target.value })}
-      />
-      {tip ? <div>Tooltip</div> : null}
-    </FormControl>
-  ) : (
-    children[0]
-  )
-}
-
-export default ItemPortal
+export default ItemPortal;
 
 /**
+ * Future:
+ *
  * Make height zero instead of not rendering, maybe?
  *
- * Future stretch goal: Input Factory, with info about inputs from data
+ * Input Factory, with info about inputs from data
+ *
+ * Cute rounded border around info
+ *
+ * Replace Typography with Theme
+ *
+ * What to do about Year / Make of product? Where to display?
+ *  For now people can just write it in notes or include in name
+ *
+ * Replace native date picker with /pickers
+ *
+ * =====
+ *
+ * CAPS for brand and product name
+ *
+ * *SOOOO* many UI questions, it's ridiculous.
+ * For now implementing most straightforward - ternary on edit
+ * Later ask someone how UI might actually look harmonious between display and edit
  */
