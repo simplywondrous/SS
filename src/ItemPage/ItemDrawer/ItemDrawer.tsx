f6a62b948f1e69e980a6691ab17154c24ff5dc31
Jasmine <workworkwang@gmail.com>
2021-01-24 18:01:08 -0600

    Removing code

--- src/ItemPage/ItemDrawer/ItemDrawer.tsx
+++ src/ItemPage/ItemDrawer/ItemDrawer.tsx
@@ -1,6 +1,4 @@
-import React, { useCallback, useRef, useState } from "react";
-
-import { useDrag, useDrop } from "react-dnd";
+import React, { useState } from "react";
 
 import { ItemPortal } from "../ItemPortal";
 import { ItemCard } from "../ItemCard/ItemCard";
@@ -9,12 +7,6 @@ import { useStyles } from "./styles";
 
 import { Drawer, Item } from "../../App/types";
 import { ItemDrawerHeading } from "./ItemDrawerHeading";
-import {
-  DraggedDrawerData,
-  DragType,
-  handleDragItemReorder,
-  handleHoverItem,
-} from "../drag-helpers";
 
 interface ItemDrawerProps {
   drawer: Drawer;
@@ -22,62 +14,21 @@ interface ItemDrawerProps {
   onDrag: (dragIndex: number, hoverIndex: number) => void;
 }
 
-export const ItemDrawer = ({ drawer, index, onDrag }: ItemDrawerProps) => {
+export const ItemDrawer = ({ drawer, index }: ItemDrawerProps) => {
   const [expanded, setExpanded] = useState(false); // If drawer is open
   const [expandedItemPortalId, setExpandedItemPortalId] = useState<
     Item["id"] | undefined
   >(undefined);
   const [drawerItems, setDrawerItems] = useState(drawer.items);
-  const dragRef = useRef<HTMLButtonElement>(null);
-  const drawerRef = useRef<HTMLDivElement>(null);
 
   const { name: drawerName, id: drawerId } = drawer;
   const { root, content, portal } = useStyles();
-
-  const [, drop] = useDrop({
-    accept: DragType.DRAWER,
-    hover: (dragged: DraggedDrawerData, monitor) =>
-      handleHoverItem(
-        dragged,
-        monitor,
-        drawerRef.current,
-        index,
-        onDrag,
-        "vertical"
-      ),
-  });
-
-  const [{ isDragging }, drag, preview] = useDrag<
-    DraggedDrawerData,
-    unknown,
-    { isDragging: boolean }
-  >({
-    item: { id: drawerId, index, type: DragType.DRAWER },
-    options: { dropEffect: "grabbing" },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-    begin: () => setExpanded(false),
-  });
 
   const handleItemClick = (id: Item["id"]) => {
     id === expandedItemPortalId
       ? setExpandedItemPortalId(undefined)
       : setExpandedItemPortalId(id);
   };
-
-  const handleItemCardDrag = useCallback(
-    (dragIndex: number, hoverIndex: number) => {
-      const newDrawerItems = handleDragItemReorder(
-        drawerItems,
-        dragIndex,
-        hoverIndex
-      );
-      setDrawerItems(newDrawerItems);
-      setExpandedItemPortalId(undefined);
-    },
-    [drawerItems]
-  );
 
   const expandedItem = drawerItems.find(
     (item) => item.id === expandedItemPortalId
@@ -89,19 +40,14 @@ export const ItemDrawer = ({ drawer, index, onDrag }: ItemDrawerProps) => {
       handleClick={handleItemClick}
       key={item.id}
       index={index}
-      onDrag={handleItemCardDrag}
     />
   ));
 
-  drag(dragRef);
-  drop(drawerRef);
-  preview(null);
   return (
     <div
       className={root}
-      ref={drawerRef}
       style={{
-        opacity: isDragging ? 0.4 : 1, // TODO: replace with moving animation
+        // TODO: replace with moving animation
         border: "1px dashed gray",
       }}
     >
@@ -110,7 +56,6 @@ export const ItemDrawer = ({ drawer, index, onDrag }: ItemDrawerProps) => {
         drawerId={drawerId}
         expanded={expanded}
         onClick={() => setExpanded(!expanded)}
-        dragRef={dragRef}
       />
       {expanded && (
         <>
